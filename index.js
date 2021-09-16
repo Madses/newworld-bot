f7259cf857d74cd811f33afa2bb723b06a95e1af
Madses <k.l.m.aartsen@hotmail.com>
2021-09-16 22:43:04 +0200

    code style fix

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const {Intents} = Discord;
 const bot = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-
-
 const TOKEN = process.env.TOKEN;
-
-
 bot.login(TOKEN);
 
 bot.on('ready', () => {

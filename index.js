838da866b792d3451fc8ed358e365b47e02b0ce8
Madses <k.l.m.aartsen@hotmail.com>
2021-09-16 22:42:33 +0200

    fixed intents error

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const {Intents} = Discord;
+const bot = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+
+const TOKEN = process.env.TOKEN;
+
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
+});

d64b26dc8282896a25f1940d4b722815cc845455
Madses <k.l.m.aartsen@hotmail.com>
2021-09-16 23:11:39 +0200

    started working on some commands

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const {Intents} = Discord;
-const bot = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const bot = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 const TOKEN = process.env.TOKEN;
 bot.login(TOKEN);
 
 bot.on('ready', () => {
     console.info(`Logged in as ${bot.user.tag}!`);
 });
+
+const prefix = "!";
+
+bot.on('message' , message => {
+    if (!message.content.startsWith(prefix)) return;
+
+    if (message.content.startsWith(prefix + "find-actives")) {
+        message.channel.send("checking for actives");
+    } 
+
+    console.log(message.content)
+})
